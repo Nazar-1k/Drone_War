e284ac7f568a13355352ff7ea2f9d69c46e642de
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-06-05 09:11:52 +0300

    try_lock

--- Concurrency/Mutex/main.cpp
+++ Concurrency/Mutex/main.cpp
@@ -4,31 +4,32 @@
 
 std::mutex mtx;
 
-void printMessage(const std::string& message) {
-    if (mtx.try_lock()) {
-        // If the lock is acquired successfully, execute the critical section
-        for (int i = 0; i < 5; ++i) {
-            std::cout << message << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
+void printMessage(const std::string& message) 
+{
+	if (mtx.try_lock())
+	{
+		// If the lock is acquired successfully, execute the critical section
+		for (int i = 0; i < 5; ++i) {
+			std::cout << message << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
 
-        mtx.unlock();
-    }
-    else {
-        // If the lock cannot be acquired, do something else
-        std::cout << "Failed to acquire the lock for message: " << message << std::endl;
-    }
+		mtx.unlock();
+	}
+	else
+	{
+		// If the lock cannot be acquired, do something else
+		std::cout << "Failed to acquire the lock for message: " << message << std::endl;
+	}
 }
 
-int main() {
-    std::thread t1(printMessage, "Thread 1 says hello!");
-    std::thread t2(printMessage, "Thread 2 says hi!");
+int main()
+{
+	std::thread t1(printMessage, "Thread 1 says hello!");
+	std::thread t2(printMessage, "Thread 2 says hi!");
 
-    t1.join();
-    t2.join();
+	t1.join();
+	t2.join();
 
-    std::mutex::native_handle_type handle = mtx.native_handle();
-    std::cout << "Native handle of the mutex: " << handle << std::endl;
-
-    return 0;
+	return 0;
 }
