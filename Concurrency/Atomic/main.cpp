19b1d08670abae4b2069b41cedf58a556ab6e83f
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-06-06 12:52:38 +0300

    Create simple exmple for atomic

---
+++ Concurrency/Atomic/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+
+std::atomic<int> counter(0);
+
+void incrementCounter()
+{
+    for (int i = 0; i < 1000000; ++i)
+    {
+        counter.fetch_add(1, std::memory_order_relaxed);
+    }
+}
+
+int main()
+{
+    std::thread thread1(incrementCounter);
+    std::thread thread2(incrementCounter);
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << "Counter value: " << counter << std::endl;
+
+    return 0;
+}
