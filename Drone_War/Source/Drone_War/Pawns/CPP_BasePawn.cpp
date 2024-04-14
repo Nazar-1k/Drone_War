37dd4cffa04d6ead5f5ebc8d6907aace249e1961
Nazar <82716260+Nazar-1k@users.noreply.github.com>
2024-04-14 16:38:03 +0300

    Implemented Base Pawn

---
+++ Drone_War/Source/Drone_War/Pawns/CPP_BasePawn.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_BasePawn.h"
+
+#include "Components/SphereComponent.h"
+
+#include "../Components/CPP_HealthComponent.h"
+#include "../Projectile/CPP_Projectile.h"
+
+// Sets default values
+ACPP_BasePawn::ACPP_BasePawn()
+{
+	// Create ProjectileSpawnPoint
+	ProjectileSpawnPoint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Spawn Point"));
+
+	// Create Sphere Component
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
+	
+	// Health Component
+	HealthComponent = CreateDefaultSubobject<UCPP_HealthComponent>(TEXT("Health Component"));
+}
+
+void ACPP_BasePawn::Shoot()
+{
+	FVector StartLocation = ProjectileSpawnPoint->GetComponentLocation();
+	FRotator RotationAngle = ProjectileSpawnPoint->GetComponentRotation();
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+
+	if (UWorld* World = GetWorld())
+	{
+		World->SpawnActor<ACPP_Projectile>(ProjectileClass, StartLocation, RotationAngle, SpawnParams);
+	}
+}
+
+void ACPP_BasePawn::EndShoot()
+{
+	CanShoot = true;
+	GetWorldTimerManager().ClearTimer(ShotTimerHandle);
+}
+
+void ACPP_BasePawn::AddAmmo(int32 CountAmmoAdded)
+{
+	CountAmmo += CountAmmoAdded;
+	OnShootDelegate.Broadcast(CountAmmo);
+}
+
+
---
+++ Drone_War/Source/Drone_War/Pawns/CPP_BasePawn.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CPP_BasePawn.generated.h"
+
+class USphereComponent;
+class UCPP_HealthComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnShootSignature, int32, CountAmmo);
+
+UCLASS()
+class DRONE_WAR_API ACPP_BasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	ACPP_BasePawn();
+
+public:
+
+	virtual void Shoot();
+	virtual void EndShoot();
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FOnShootSignature OnShootDelegate;
+
+public:
+
+	FORCEINLINE bool IsLoose() { return bLoose; }
+	FORCEINLINE UCPP_HealthComponent* GetHealthComponent() { return HealthComponent; }
+	FORCEINLINE int32 GetCountAmmo() { return CountAmmo; }
+
+	void AddAmmo(int32 CountAmmoAdded);
+
+protected:
+
+	/** Health Component */
+	UPROPERTY(EditAnywhere, Category = "Components")
+	UCPP_HealthComponent* HealthComponent;
+
+	/** True if you Loose */
+	bool bLoose = false;
+
+	/** Shot Range */
+	UPROPERTY(EditAnywhere, Category = "Settings|Shoot")
+	float FireRate = 0.5f;
+
+	/** Count Ammo */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Settings|Shoot", meta = (AllowPrivateAccess = "true"))
+	int32 CountAmmo = 10;
+
+	bool CanShoot = true;
+	FTimerHandle ShotTimerHandle;
+
+protected:
+
+	/** Base Skeletal Mesh Component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USphereComponent* SphereComponent;
+
+	/** Actor Class for Projectile */
+	UPROPERTY(EditAnywhere, Category = "Settings|Projectile", meta = (AllowPrivateAccess = "true"))
+	class UClass* ProjectileClass;
+
+	/** Projectile Spawn Point */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* ProjectileSpawnPoint;
+
+};
