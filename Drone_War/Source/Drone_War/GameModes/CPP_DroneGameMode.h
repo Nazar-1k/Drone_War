1ce6ceb950ebd4849ee37f4b90bd3906e50be74b
Nazar <82716260+Nazar-1k@users.noreply.github.com>
2024-04-14 16:40:20 +0300

    Added Game Mode

---
+++ Drone_War/Source/Drone_War/GameModes/CPP_DroneGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_DroneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONE_WAR_API ACPP_DroneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
